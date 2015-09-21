6378c9fa818e306dc85fbca61abcdd71199fe0e1
giwankim <giwan.kim@gmail.com>
2015-09-21 17:35:02 -0400

    Let's go

--- cartoonCollections/FISAppDelegate.h
+++ cartoonCollections/FISAppDelegate.h
@@ -18,4 +18,14 @@
  
  */
 
+-(NSString *)stringByRollCallingDwarfsInArray:(NSArray *)dwarfs;
+
+-(NSArray *)arrayOfPlaneteerShoutsFromArray:(NSArray *)powers;
+
+-(NSString *)summonCaptainPlanetWithPowers:(NSArray *)powers;
+
+-(NSString *)firstPremiumCheeseInStock:(NSArray *)cheesesInStock premiumCheeseNames:(NSArray *)premiumCheeseNames;
+
+-(NSArray *)arrayByConvertingMoneyBagsIntoPaperBills:(NSArray *)moneyBags;
+
 @end
