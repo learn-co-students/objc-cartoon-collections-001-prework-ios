a337cc9561c7129de83cd75ab6c5953ce35e9b1e
dmead28 <dmead28@gmail.com>
2015-12-24 16:23:58 -0500

    Complete: dmead

--- cartoonCollections/FISAppDelegate.m
+++ cartoonCollections/FISAppDelegate.m
@@ -9,10 +9,72 @@
     return YES;
 }
 
-/**
- 
- *  Define your method implementations here.
- 
- */
+
+-(NSString*)stringByRollCallingDwarfsInArray:(NSArray*)dwarfs {
+    NSMutableString *dwarfRollCall = [[NSMutableString alloc] init];
+    NSUInteger index = 1;
+    for (NSUInteger i = 0; i < [dwarfs count]; i++) {
+        if (i == 0) {
+            [dwarfRollCall appendFormat:@"%lu. %@",index,dwarfs[i]];
+        } else {
+            [dwarfRollCall appendFormat:@" | %lu. %@",index,dwarfs[i]];
+        }
+        index++;
+    }
+    
+    return dwarfRollCall;
+}
+-(NSArray*)arrayOfPlaneteerShoutsFromArray:(NSArray*)powers {
+    NSMutableArray *powersCombine = [[NSMutableArray alloc] init];
+    NSString *powerString;
+    
+    for (NSUInteger i = 0; i < [powers count]; i++) {
+        powerString = [NSString stringWithFormat:@"%@!",[powers[i] uppercaseString]];
+        [powersCombine addObject:powerString];
+    }
+    
+    return powersCombine;
+}
+-(NSString*)summonCaptainPlanetWithPowers:(NSArray*)powers {
+    NSMutableString *powersCombineString = [[NSMutableString alloc] init];
+    [powersCombineString appendString:@"Let our powers combine:\n"];
+    
+    NSArray *powersFormattedArray = [self arrayOfPlaneteerShoutsFromArray:powers];
+    
+    for (NSUInteger i = 0; i < [powersFormattedArray count]; i++) {
+        [powersCombineString appendFormat:@"%@\n",powersFormattedArray[i]];
+    }
+    
+    [powersCombineString appendString:@"Go Planet!"];
+    
+    return powersCombineString;
+}
+-(NSString*)firstPremiumCheeseInStock:(NSArray*)cheesesInStock premiumCheeseNames:(NSArray*)premiumCheeseNames {
+    for (NSUInteger i = 0; i < [premiumCheeseNames count]; i++) {
+        for (NSUInteger j = 0; j < [cheesesInStock count]; j++) {
+            if ([premiumCheeseNames[i] isEqualToString:cheesesInStock[j]]) {
+                return premiumCheeseNames[i];
+            }
+        }
+    }
+    return @"No premium cheeses in stock.";
+}
+-(NSArray*)arrayByConvertingMoneyBagsIntoPaperBills:(NSArray*)moneyBags {
+    NSMutableArray *outputArray = [[NSMutableArray alloc] init];
+    //NSString currentString;
+    for (NSUInteger i = 0; i < [moneyBags count]; i++) {
+        NSString *dollarString = [NSString stringWithFormat:@"$%lu", [moneyBags[i] length]];
+        [outputArray addObject:dollarString];
+    }
+    return outputArray;
+}
+
+
+
+
+
+
+
+
 
 @end
