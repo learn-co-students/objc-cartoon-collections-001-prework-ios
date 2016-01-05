a960797575dfc72e4ac72dd9aec7ce41f20eb475
kenmhaggerty <kenmhaggerty@gmail.com>
2016-01-05 14:18:16 -0500

    Stubs out methods for FISAppDelegate.

--- cartoonCollections/FISAppDelegate.m
+++ cartoonCollections/FISAppDelegate.m
@@ -9,10 +9,29 @@
     return YES;
 }
 
-/**
- 
- *  Define your method implementations here.
- 
- */
+- (NSString *)stringByRollCallingDwarfsInArray:(NSArray *)dwarfs {
+    
+    return nil;
+}
+
+- (NSArray *)arrayOfPlaneteerShoutsFromArray:(NSArray *)powers {
+    
+    return nil;
+}
+
+- (NSString *)summonCaptainPlanetWithPowers:(NSArray *)powers {
+    
+    return nil;
+}
+
+- (NSString *)firstPremiumCheeseInStock:(NSArray *)cheesesInStock premiumCheeseNames:(NSArray *)premiumCheeseNames {
+    
+    return nil;
+}
+
+- (NSArray *)arrayByConvertingMoneyBagsIntoPaperBills:(NSArray *)moneyBags {
+    
+    return nil;
+}
 
 @end
