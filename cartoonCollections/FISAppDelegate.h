a03f6073861403319cae75ec3fc51fed155b95c0
kevinkong91 <kevinkong91@gmail.com>
2016-01-19 13:58:05 -0500

    cartoon collections

--- cartoonCollections/FISAppDelegate.h
+++ cartoonCollections/FISAppDelegate.h
@@ -6,10 +6,20 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-/**
- 
- * Declare your methods here.
- 
- */
+
+-(NSString *)stringByRollCallingDwarfsInArray:(NSArray *)dwarfs;
+-(NSArray *)arrayOfPlaneteerShoutsFromArray:(NSArray *)powers;
+-(NSString *)summonCaptainPlanetWithPowers:(NSArray *)powers;
+-(NSString *)firstPremiumCheeseInStock:(NSArray *)cheesesInStock premiumCheeseNames:(NSArray *)premiumCheeseNames;
+-(NSArray *)arrayByConvertingMoneyBagsIntoPaperBills:(NSArray *)moneyBags;
+
+/*
+stringByRollCallingDwarfsInArray: that takes one NSArray argument called dwarfs and returns an NSString.
+arrayOfPlaneteerShoutsFromArray: that takes one NSArray argument called powers and returns an NSArray.
+summonCaptainPlanetWithPowers: that takes one NSArray argument called powers and returns an NSString.
+firstPremiumCheeseInStock:premiumCheeseNames: that takes two NSArray arguments called cheesesInStock and premiumCheeseNames, and returns an NSString.
+arrayByConvertingMoneyBagsIntoPaperBills: that takes one NSArray argument moneyBags and returns an NSArray.
+*/
+
 
 @end
