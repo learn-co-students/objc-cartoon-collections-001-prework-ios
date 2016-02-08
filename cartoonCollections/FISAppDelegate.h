97e23455e43ed51f4d94d5a384a00c7abeee56c4
terence4wilbert <terence4wilbert@gmail.com>
2016-02-08 09:40:31 -0600

    cartoom collection

--- cartoonCollections/FISAppDelegate.h
+++ cartoonCollections/FISAppDelegate.h
@@ -12,4 +12,8 @@
  
  */
 
-@end
+-(NSString *)stringByRollCallingDwarfsInArray:(NSArray *)dwarfs;
+-(NSArray *)arrayOfPlaneteerShoutsFromArray:(NSArray *)powers;
+-(NSString *)summonCaptainPlanetWithPowers:(NSArray * )powers;
+-(NSString *)firstPremiumCheeseInStock:(NSArray *)premiumCheeses premiumCheeseNames:(NSArray *)cheesesInStock;
+-(NSArray *)arrayByConvertingMoneyBagsIntoPaperBills:(NSArray *)moneyBags;@end
